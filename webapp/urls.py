83888c99ef1aa5f633dd87fc6e784fec11676c02
Ekaterina Gudkova <katiegudkova2009@gmail.com>
2026-05-28 12:28:02 +0600

    Add task detail page

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from webapp.views import task_list, task_add, task_delete
+from webapp.views import task_list, task_add, task_delete, task_detail
 
 urlpatterns = [
     path('tasks/', task_list, name='task_list'),
     path('tasks/add/', task_add, name='task_add'),
+    path('tasks/<int:pk>/', task_detail, name='task_detail'),
     path('tasks/<int:pk>/delete/', task_delete, name='task_delete'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -7,21 +7,28 @@ def task_list(request):
     return render(request, 'webapp/task_list.html', {'tasks': tasks})
 
 
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'webapp/task_detail.html', {'task': task})
+
+
 def task_add(request):
     if request.method == 'POST':
         description = request.POST.get('description')
+        detailed_description = request.POST.get('detailed_description')
         status = request.POST.get('status')
         due_date = request.POST.get('due_date') or None
         Task.objects.create(
             description=description,
+            detailed_description=detailed_description,
             status=status,
             due_date=due_date
         )
-        return redirect('/tasks/')
+        return redirect('task_list')
     return render(request, 'webapp/task_add.html', {'status_choices': STATUS_CHOICES})
 
 
 def task_delete(request, pk):
     task = get_object_or_404(Task, pk=pk)
     task.delete()
-    return redirect('/tasks/')
+    return redirect('task_list')
